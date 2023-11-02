2a5a1beed498e63c366599b9f7b23c72d11b6a2d
rijalllhd <ijal1445b@gmail.com>
2023-11-02 11:27:28 +0700

    login admin berserta middleware, template sudah di section

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use Auth;
+use Carbon\carbon;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    public function Login(){
+        return view('logindanregis.loginadmin');
+    }
+
+    public function Dashboard(){
+        return view('admin.dashboard');
+    }
+
+    public function LoginProses(Request $request){
+        // dd($request->all());
+        $check = $request->all();
+        if (Auth::guard('admin')->attempt(['username'=>$check['username'], 'password'=>$check['password']])) {
+            session()->regenerate();
+            return redirect()->route('admin.dashboard');
+        } else {
+            return back();
+        }
+    }
+
+    public function Logout() {
+        Auth::guard('admin')->logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        return redirect()->route("formlogin_admin");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +20,18 @@ Route::get('/', function () {
 });
 
 Route::prefix('user')->group(function(){
-    Route::get('/login',[UserController::class, 'Index'])->name('formlogin_user');
+    Route::get('/login',[UserController::class, 'Index'])->name('formlogin_user')->middleware('guest');
     Route::post('/login/proses',[UserController::class, 'Login'])->name('user.login');
     Route::get('/dashboard',[UserController::class, 'Dashboard'])->name('user.dashboard')->middleware('auth');
     Route::get('/logout',[UserController::class, 'Logout'])->name('user.logout')->middleware('auth');
     Route::get('/register',[UserController::class, 'Register'])->name('user.register')->middleware('guest');
     Route::post('/register/proses',[UserController::class, 'RegisterCreate'])->name('user.register.create');
+});
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('/login', [AdminController::class, 'Login'])->name('formlogin_admin')->middleware('guest');
+    Route::post('/login/proses', [AdminController::class, 'LoginProses'])->name('admin.login');
+    Route::get('/dashboard',[AdminController::class, 'Dashboard'])->name('admin.dashboard')->middleware('admin');
+    Route::get('/logout', [AdminController::class, 'Logout'])->name('admin.logout')->middleware('admin');
 });
